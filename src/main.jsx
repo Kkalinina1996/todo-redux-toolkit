f2d935f3e3a9c3be7ffe82bacf9724d59e7b1b9d
Kristine Kalinina <kviljeva@gmail.com>
2026-01-14 18:30:19 +0100

    Update files

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,9 @@
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-import { Provider } from "react-redux";
-import store from "./redux/store.js";
-import 'rsuite/dist/rsuite.min.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
-
-
-createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </Provider>
-);
+  </React.StrictMode>
+)
